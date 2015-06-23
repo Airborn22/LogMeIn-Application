4f3a378785c610bd5d60eb7b58e11a31bf8a3641
Sandor Major <ms@totaldeluxe.com>
2015-06-23 19:45:12 +0200

    occupation test case updated

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -10,21 +10,42 @@ var expect = require('expect.js');
 describe('api test', function(){
   // Test the occupation autocomplete with an empty string
   it('occupation autocomplate empty', function(done){
-    superagent.get('http://localhost:8888/v1/occupation/')
+    superagent.get('http://localhost:8888/v1/occupation/').accept('application/json')
       .end(function(error, response) {
         expect(error).to.eql(null);
-        expect(typeof response.body).to.eql('object');
+        expect(response.body).to.be.an('array');
+        expect(response.body).to.eql([
+          'Application analyst',
+          'Computer operator',
+          'Computer repair technician',
+          'Computer scientist',
+          'Computer analyst',
+          'Data entry clerk',
+          'Database administrator',
+          'Data analyst',
+          'Data scientist',
+          'Network analyst',
+          'Network administrator',
+          'Programmer',
+          'Security engineer'
+        ]);
 
         done();
       });
   });
 
-  // Test the occupation autocomplete with "a"
+  // Test the occupation autocomplete with "c"
   it('occupation autocomplate "a"', function(done){
-    superagent.get('http://localhost:8888/v1/occupation/a')
+    superagent.get('http://localhost:8888/v1/occupation/a').accept('application/json')
       .end(function(error, response) {
         expect(error).to.eql(null);
-        expect(typeof response.body).to.eql('object');
+        expect(response.body).to.be.an('array');
+        expect(response.body).to.eql([
+          'Computer operator',
+          'Computer repair technician',
+          'Computer scientist',
+          'Computer analyst'
+        ]);
 
         done();
       });
