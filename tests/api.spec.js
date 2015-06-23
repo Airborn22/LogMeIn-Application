6bce354524dbf72c215683d7ae163dd661d20524
Sandor Major <ms@totaldeluxe.com>
2015-06-23 19:01:37 +0200

    api test base created

---
+++ tests/api.spec.js
@@ -0,0 +1,11 @@
+/*jslint node: true, indent: 2 */
+/*global describe, it, before, beforeEach, after, afterEach */
+'use strict';
+var superagent = require('superagent');
+var expect = require('expect.js');
+
+/**
+ * Test for the REST API
+ */
+describe('api test', function(){
+});
