b672e3f53fd806da052c8c4c1e46731eb755760b
Sandor Major <ms@totaldeluxe.com>
2015-06-23 19:10:52 +0200

    occupation autocomplete test base

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -8,4 +8,21 @@ var expect = require('expect.js');
  * Test for the REST API
  */
 describe('api test', function(){
+  // Test the occupation autocomplete with an empty string
+  it('occupation autocomplate empty', function(done){
+    superagent.get('http://localhost:8888/v1/occupation/')
+      .end(function(error, response) {
+        expect(error).to.eql(null);
+        expect(typeof response.body).to.eql('object');
+      });
+  });
+
+  // Test the occupation autocomplete with "a"
+  it('occupation autocomplate "a"', function(done){
+    superagent.get('http://localhost:8888/v1/occupation/a')
+      .end(function(error, response) {
+        expect(error).to.eql(null);
+        expect(typeof response.body).to.eql('object');
+      });
+  });
 });
